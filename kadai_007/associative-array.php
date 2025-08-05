15875718f59243947713f62510aa5198ed6d5006
user13674 <sgnak1010@gmail.com>
2025-08-05 23:53:41 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>連想配列を作ってキーと値を出力しよう</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    $product_array = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    print_r($product_array);
+    ?>
+  </p>
+</body>
+
+</html>
